4741b3c21e17e4d88dba3628d0b4a005d2c3f7d8
William Koller <wkoller25@gmail.com>
2020-11-19 00:58:01 -0300

    feat: create migrations

---
+++ src/database/migrations/1605757094492-CreateLessons.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export default class CreateLessons1605757094492 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'lessons',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'descirption',
+            type: 'varchar',
+          },
+          {
+            name: 'created_At',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_At',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('lessons')
+  }
+}
