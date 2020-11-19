3796ddd43c5d6d90e8329aa06aaf4210bda77f12
William Koller <wkoller25@gmail.com>
2020-11-19 00:35:14 -0300

    feat: add migration create class

---
+++ src/database/migrations/1605755282793-createClass.ts
@@ -0,0 +1,42 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export default class CreateClass1605755282793 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'class',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'duration',
+            type: 'integer',
+          },
+          {
+            name: 'created_At',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_At',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('class')
+  }
+}
