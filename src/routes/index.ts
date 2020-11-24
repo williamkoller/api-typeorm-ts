5d1c41c60eb691ecb58441271a06eecdb55a26b0
William Koller <wkoller25@gmail.com>
2020-11-23 23:30:26 -0300

    feat: add route class

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
+import classRouter from './class.routes'
 
 const routes = Router()
 
-routes.use('/class')
+routes.use('/class', classRouter)
 
 export default routes
