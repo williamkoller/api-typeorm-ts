42126dfa6f18e1e020c265d90f4e7d4f95b93c69
William Koller <wkoller25@gmail.com>
2020-11-23 22:23:49 -0300

    feat: add model Class

---
+++ src/models/Class.ts
@@ -0,0 +1,19 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
+
+@Entity()
+export default class Class {
+  @PrimaryGeneratedColumn('uuid')
+  id: string
+
+  @Column({ length: 100, unique: true })
+  name: string
+
+  @Column()
+  duration: number
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updatedAt: Date
+}
