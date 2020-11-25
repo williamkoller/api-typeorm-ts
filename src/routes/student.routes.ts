b4aa10dfac7e15cdf09c1cbf10c8c891fb42ccd0
William Koller <wkoller25@gmail.com>
2020-11-24 22:48:45 -0300

    refactor: add validate class-validator

--- src/routes/student.routes.ts
+++ src/routes/student.routes.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express'
 import { getRepository } from 'typeorm'
+import { validate } from 'class-validator'
 import Student from '../models/Student'
 
 const studentRouter = Router()
@@ -9,8 +10,19 @@ studentRouter.post(
   async (request: Request, response: Response): Promise<Response> => {
     try {
       const repo = getRepository(Student)
-      const res = repo.save(request.body)
-      return response.status(201).json(res)
+      const { name, key, email } = request.body
+      const student = repo.create({
+        name,
+        key,
+        email,
+      })
+      const erros = await validate(student)
+
+      if (erros.length === 0) {
+        const res = await repo.save(student)
+        return response.status(201).json(res)
+      }
+      return response.status(400).json(erros)
     } catch (error) {
       console.log('error.message: ', error.message)
       return response.status(400).send({
