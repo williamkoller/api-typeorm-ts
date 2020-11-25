009dd7a64fd286368dcde24bb175b4491b9972e4
William Koller <wkoller25@gmail.com>
2020-11-24 22:47:31 -0300

    refactor: alter migrations

--- src/database/migrations/1606268356863-Relations.ts
+++ src/database/migrations/1606268356863-Relations.ts
@@ -1,20 +1,15 @@
 import { MigrationInterface, QueryRunner } from 'typeorm'
 
-export default class RelationClassStudent1606193105646 implements MigrationInterface {
-  name = 'RelationClassStudent1606193105646'
+export default class Relations1606268356863 implements MigrationInterface {
+  name = 'Relations1606268356863'
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE "lesson" DROP CONSTRAINT "FK_4b7d004f5939b7d7f4238526680"`)
     await queryRunner.query(
       `CREATE TABLE "student_classes_class" ("studentId" uuid NOT NULL, "classId" uuid NOT NULL, CONSTRAINT "PK_9f2c655a5feb7cff342af016c8e" PRIMARY KEY ("studentId", "classId"))`,
     )
     await queryRunner.query(`CREATE INDEX "IDX_4e224193a4e2c8e1b28afa74e9" ON "student_classes_class" ("studentId") `)
     await queryRunner.query(`CREATE INDEX "IDX_3d4b9aa106e0113abd39f06182" ON "student_classes_class" ("classId") `)
-    await queryRunner.query(`ALTER TABLE "lesson" DROP CONSTRAINT "REL_4b7d004f5939b7d7f423852668"`)
-    await queryRunner.query(`ALTER TABLE "lesson" DROP COLUMN "contentId"`)
-    await queryRunner.query(
-      `ALTER TABLE "student_classes_class" ADD CONSTRAINT "FK_4e224193a4e2c8e1b28afa74e9d" FOREIGN KEY ("studentId") REFERENCES "student"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
-    )
+
     await queryRunner.query(
       `ALTER TABLE "student_classes_class" ADD CONSTRAINT "FK_3d4b9aa106e0113abd39f061827" FOREIGN KEY ("classId") REFERENCES "class"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
     )
@@ -22,14 +17,11 @@ export default class RelationClassStudent1606193105646 implements MigrationInter
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "student_classes_class" DROP CONSTRAINT "FK_3d4b9aa106e0113abd39f061827"`)
-    await queryRunner.query(`ALTER TABLE "student_classes_class" DROP CONSTRAINT "FK_4e224193a4e2c8e1b28afa74e9d"`)
+    await queryRunner.query(`ALTER TABLE "student" DROP CONSTRAINT "UQ_a56c051c91dbe1068ad683f536e"`)
     await queryRunner.query(`ALTER TABLE "lesson" ADD "contentId" uuid`)
     await queryRunner.query(`ALTER TABLE "lesson" ADD CONSTRAINT "REL_4b7d004f5939b7d7f423852668" UNIQUE ("contentId")`)
     await queryRunner.query(`DROP INDEX "IDX_3d4b9aa106e0113abd39f06182"`)
     await queryRunner.query(`DROP INDEX "IDX_4e224193a4e2c8e1b28afa74e9"`)
     await queryRunner.query(`DROP TABLE "student_classes_class"`)
-    await queryRunner.query(
-      `ALTER TABLE "lesson" ADD CONSTRAINT "FK_4b7d004f5939b7d7f4238526680" FOREIGN KEY ("contentId") REFERENCES "content"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
-    )
   }
 }
--- src/database/migrations/1606268709724-RelationsStudentLesson.ts
+++ src/database/migrations/1606268709724-RelationsStudentLesson.ts
@@ -1,17 +1,17 @@
 import { MigrationInterface, QueryRunner } from 'typeorm'
 
-export default class RelationLessonClass1606189371484 implements MigrationInterface {
-  name = 'RelationLessonClass1606189371484'
+export default class RelationsStudentLesson1606268709724 implements MigrationInterface {
+  name = 'RelationsStudentLesson1606268709724'
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "content" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "key" integer NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "lessonId" uuid, CONSTRAINT "REL_0b349f6b8ca7f05eed39ffb956" UNIQUE ("lessonId"), CONSTRAINT "PK_6a2083913f3647b44f205204e36" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "content" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "description" character varying NOT NULL, "link_content" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "lessonId" uuid, CONSTRAINT "REL_0b349f6b8ca7f05eed39ffb956" UNIQUE ("lessonId"), CONSTRAINT "PK_6a2083913f3647b44f205204e36" PRIMARY KEY ("id"))`,
     )
     await queryRunner.query(
-      `CREATE TABLE "lesson" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "description" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "contentId" uuid, "classesId" uuid, CONSTRAINT "REL_4b7d004f5939b7d7f423852668" UNIQUE ("contentId"), CONSTRAINT "PK_0ef25918f0237e68696dee455bd" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "lesson" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "description" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "classesId" uuid, CONSTRAINT "PK_0ef25918f0237e68696dee455bd" PRIMARY KEY ("id"))`,
     )
     await queryRunner.query(
-      `CREATE TABLE "student" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "description" character varying NOT NULL, "link_content" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_3d8016e1cb58429474a3c041904" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "student" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "key" integer NOT NULL, "email" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_a56c051c91dbe1068ad683f536e" UNIQUE ("email"), CONSTRAINT "PK_3d8016e1cb58429474a3c041904" PRIMARY KEY ("id"))`,
     )
     await queryRunner.query(`ALTER TABLE "class" DROP COLUMN "name"`)
     await queryRunner.query(`ALTER TABLE "class" ADD "name" character varying(100) NOT NULL`)
@@ -19,15 +19,17 @@ export default class RelationLessonClass1606189371484 implements MigrationInterf
     await queryRunner.query(
       `ALTER TABLE "content" ADD CONSTRAINT "FK_0b349f6b8ca7f05eed39ffb956d" FOREIGN KEY ("lessonId") REFERENCES "lesson"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
     )
-
     await queryRunner.query(
       `ALTER TABLE "lesson" ADD CONSTRAINT "FK_24df875df1c911a41db18ff8773" FOREIGN KEY ("classesId") REFERENCES "class"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "student_classes_class" ADD CONSTRAINT "FK_4e224193a4e2c8e1b28afa74e9d" FOREIGN KEY ("studentId") REFERENCES "student"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
     )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "student_classes_class" DROP CONSTRAINT "FK_4e224193a4e2c8e1b28afa74e9d"`)
     await queryRunner.query(`ALTER TABLE "lesson" DROP CONSTRAINT "FK_24df875df1c911a41db18ff8773"`)
-    await queryRunner.query(`ALTER TABLE "lesson" DROP CONSTRAINT "FK_4b7d004f5939b7d7f4238526680"`)
     await queryRunner.query(`ALTER TABLE "content" DROP CONSTRAINT "FK_0b349f6b8ca7f05eed39ffb956d"`)
     await queryRunner.query(`ALTER TABLE "class" DROP CONSTRAINT "UQ_574dd394846fb85d495d0f77dfd"`)
     await queryRunner.query(`ALTER TABLE "class" DROP COLUMN "name"`)
